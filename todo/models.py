2efc4ab56b5b27f740e1cac41f510130a3493812
wahid1099 <wahidahmed890@gmail.com>
2023-02-13 17:19:04 +0600

    adding all

---
+++ todo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    discription=models.TextField()
+    completed=models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title
+        
